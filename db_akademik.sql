85a340009a41f345ef755db0f5df000fd3507c95
Aprill33 <aprillee344@gmail.com>
2025-11-19 14:03:03 +0700

    Add files via upload

---
+++ db_akademik.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE akademik;
+USE akademik;
+
+CREATE TABLE mahasiswa(
+nim VARCHAR(11) PRIMARY KEY,
+nama_mhs VARCHAR(30),
+fakultas VARCHAR(30),
+prodi VARCHAR(30),
+alamat VARCHAR(50),
+no_hp VARCHAR(13)
+);
+
+
